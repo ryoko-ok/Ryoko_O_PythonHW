257f82b899de6507753af32cdfa20ed1b7c6746f
Oka <r_oka@ats-hr420-dm03.fc.ca>
2019-10-29 13:30:21 -0400

    updating game

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 from random import randint
 
 #choices is an array =>a arrray is a container that can hold multiple values
-choices= ["rock", "paper", "scissors"]
+choices = ["rock", "paper", "scissors"]
 
 #set the computer variable to one of these choices
 computer = choices[randint(0, 2)]
@@ -10,8 +10,9 @@ computer = choices[randint(0, 2)]
 #set up the game loop so that we don't have to restart all the time
 player = False
 
-while player is False:
+while player == False:
 #set player to True
+
 	print("*****************************\n\n")
 	print("Choose your weapon!\n\n")
 	print("*****************************\n\n")
@@ -21,31 +22,31 @@ while player is False:
 	print("computer chose ", computer, "\n")
 	print("player chose ", player, "\n")
 
-	if player == "quit":
+	if player.lower() == "quit":
 		exit()
 	elif computer == player:
 		print("tie! no one wins, play again")
-	elif player == "Rock":
-		if computer == "Paper":
+	elif player.lower() == "rock":
+		if computer == "paper":
 			print("You lose!", computer, "covers", player, "\n")
 		else:
-			prnt("You win!", player, "smashes through", computer, "\n")
+			print("You win!", player, "smashes through", computer, "\n")
 
-	elif player == "Paper":
-		if computer == "Scissors":
+	elif player.lower() == "paper":
+		if computer == "scissors":
 			print("You lose!", computer, "cuts", player, "\n")
 		else:
-			prnt("You win!", player, "smashes", computer, "\n")
+			print("You win!", player, "covers", computer, "\n")
 
-	elif player == "Scissors":
-		if computer == "Rock":
+	elif player.lower() == "scissors":
+		if computer == "rock":
 			print("You lose!", computer, "smashes", player, "\n")
 		else:
-			prnt("You win!", player, "cuts", computer, "\n")
+			print("You win!", player, "cuts", computer, "\n")
 
 	else:
 		print("That's not a valid choice, try again")
 
 # need to check all of our conditions after checking for a tie
-		player = False
-		computer = choices[randint(0, 2)]
+	player = False
+	computer = choices[randint(0, 2)]
