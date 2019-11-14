a6a5d982458098dd76f5326ecb515c48a2ac38d9
Ryoko Oka <ryooo.kooo.22@gmail.com>
2019-11-14 17:31:46 -0500

    upload last class

--- game.py
+++ game.py
@@ -1,71 +1,29 @@
 # import the randompackage so that we can generate a random choice
 from random import randint
-from gameFunctions import winlose
+from gameFunctions import winlose, gameWars, compare
+from Example import compareStuff
 
-# set up some variables for player and AI lives
-player_lives = 5
-computer_lives = 5
-
-# choices is an array =>a arrray is a container that can hold multiple values
-# arrays are 0-based -> first entry is 0, 2nd is 1, 3rd is 2 etc
-choices = ["rock", "paper", "scissors"]
-
-# set the computer variable to one of these choices
-computer = choices[randint(0, 2)]
-
-# set up the game loop so that we don't have to restart all the time
-player = False
-
-while player == False:
-#set player to True
+while gameWars.player == False:
+# set player to True
 	print("*****************************\n")
-	print("Computer lives; ", computer_lives, "/5\n")
-	print("Player lives; ", player_lives, "/5\n")
+	print("gameWars.Computer lives; ", gameWars.computer_lives, "/", gameWars.total_lives, "\n")
+	print("Player lives; ", gameWars.player_lives, "/", gameWars.total_lives,"\n")
 	print("Choose your weapon!\n")
 	print("*****************************\n")
 
 	player = input ("choose rock, paper or scissors: ")
 	player = player.lower()
 
-	print("computer chose", computer, "\n")
+	print("gameWars.computer chose", gameWars.computer, "\n")
 	print("player chose", player, "\n")
 
-	if player.lower() == "quit":
-		exit()
-
-	elif computer == player:
-		print("tie! no one wins, play again")
-
-	elif player.lower() == "rock":
-		if computer == "paper":
-			print("You lose!", computer, "covers", player, "\n")
-			player_lives = player_lives - 1
-		else:
-			print("You win!", player, "smashes through", computer, "\n")
-			computer_lives = computer_lives - 1
-
-	elif player.lower() == "paper":
-		if computer == "scissors":
-			print("You lose!", computer, "cuts", player, "\n")
-			player_lives = player_lives - 1
-		else:
-			print("You win!", player, "covers", computer, "\n")
-			computer_lives = computer_lives - 1
-
-	elif player.lower() == "scissors":
-		if computer == "rock":
-			print("You lose!", computer, "smashes", player, "\n")
-			player_lives = player_lives - 1
-		else:
-			print("You win!", player, "cuts", computer, "\n")
-			computer_lives = computer_lives - 1
-
-	else:
-		print("That's not a valid choice, try again")
+	### this is where you would call compare
+	
+	#### end compare stuff
 
 
 	# handle all lives lost for player or AI
-	if player_lives is 0:
+	if gameWars.player_lives is 0:
 		winlose.winorlose("lost")
 	# if player_lives is 0:
 	# 	print("Out of lives! you suck at this game. Would you like to play again?\n")
@@ -79,15 +37,15 @@ while player == False:
 	# 	elif (choice is "Y") or (choice is "y"):
 	# 		# reset the game so that we can start all over again
 	# 		player_lives = 5
-	# 		computer_lives = 5
+	# 		gameWars.computer_lives = 5
 	# 		player = False
-	# 		computer = choices[randint(0,2)]
+	# 		gameWars.computer = choices[randint(0,2)]
 
 
-	elif computer_lives is 0:
+	elif gameWars.computer_lives is 0:
 		winlose.winorlose("lost")
 
-		# print("Computer is out of lives! you suck at this game. Would you like to play again?\n")
+		# print("gameWars.Computer is out of lives! you suck at this game. Would you like to play again?\n")
 		# choice = input ("Y / N")
 		# print(choice)
 
@@ -98,11 +56,11 @@ while player == False:
 		# elif (choice is "Y") or (choice is "y"):
 		# 	# reset the game so that we can start all over again
 		# 	player_lives = 5
-		# 	computer_lives = 5
+		# 	gameWars.computer_lives = 5
 		# 	player = False
-		# 	computer = choices[randint(0,2)]
+		# 	gameWars.computer = choices[randint(0,2)]
 
 	else:
 		# need to check all of our conditions after checking for a tie
 		player = False
-		computer = choices[randint(0, 2)]
+		gameWars.computer = gameWars.choices[randint(0, 2)]
--- gameFunctions/winlose.py
+++ gameFunctions/winlose.py
@@ -1,4 +1,5 @@
 from random import randint
+from gameFunctions import gameWars
 # define a python function that takes an argument
 def winorlose(status):
 	# status will be either won or lost - you're passing this in as an argument
@@ -7,7 +8,7 @@ def winorlose(status):
 
 	print("You", status, "! Would you like to play again?")
 
-	choice = input ("Y / N")
+	choice = input ("Y / N: ")
 	print(choice)
 
 	if (choice is "N") or (choice is "n"):
@@ -16,13 +17,19 @@ def winorlose(status):
 
 	elif (choice is "Y") or (choice is "y"):
 		# reset the game so that we can start all over again
-		global player_lives
-		global computer_lives
-		global player
-		global computer
-		global choices
+		# player_lives
+		# computer_lives
+		# player
+		# computer
+		# choices
 
-		player_lives = 5
-		computer_lives = 5
-		player = False
-		computer = choices[randint(0,2)]
+		gameWars.player_lives = 1
+		gameWars.computer_lives = 1
+		gameWars.total_lives = 1
+		gameWars.player = False
+		gameWars.computer = gameWars.choices[randint(0,2)]
+
+	else:
+		# use recursion to call winorlose again until we get the right input
+		# recursion is just a fancy way to describe calling a function from within itself
+		winorlose(status)
